1f927a89cd7dcdd4c3bc6aa6fb3a90a2393efb76
ligy <ligy@zetyun.com>
2023-08-25 09:29:17 +0800

    Initial commit

---
+++ src/test/java/com/ligy/graduationproject/GraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ligy.graduationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
