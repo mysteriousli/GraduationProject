a2fbc642daeb146f2667eab523ed95e30240dfcc
ligy <ligy@zetyun.com>
2023-08-25 09:44:34 +0800

    添加模块

--- src/main/java/com/ligy/graduationproject/GraduationProjectApplication.java
+++ src/main/java/com/ligy/graduationproject/GraduationProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GraduationProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraduationProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationProjectApplication.class, args);
+    }
 
 }
